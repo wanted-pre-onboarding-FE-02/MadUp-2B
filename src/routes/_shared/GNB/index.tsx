7039c4634d2227b2cb965c512d735c759dbc1fb5
saiani1 <saiani@naver.com>
2022-05-23 22:43:22 +0900

    feat: custom dropdown, fix svg file code

--- src/routes/_shared/GNB/index.tsx
+++ src/routes/_shared/GNB/index.tsx
@@ -1,25 +1,73 @@
 import cx from 'classnames'
-import { NavLink } from 'react-router-dom'
+import { useState, useEffect, useRef } from 'react'
+import { NavLink, Link } from 'react-router-dom'
 import styles from './gnb.module.scss'
 
 import { LogoImage, DashboardIcon, ChartIcon, GuideIcon, ArrowIcon } from 'assets/svgs/index'
 
 const GNB = () => {
+  const dropdownRef = useRef<HTMLUListElement>(null)
+  const [selectedOption, setSelectedOption] = useState<string>('매드업')
+  const [dropdownClick, setDropdownClick] = useState(false)
+
+  const handleDropdownClick = () => {
+    setDropdownClick((prev) => {
+      return !prev
+    })
+  }
+
+  const handleOptionClick = (event: React.MouseEvent<HTMLElement>) => {
+    const targetText = (event.target as HTMLElement).textContent
+    setSelectedOption(targetText as string)
+    setDropdownClick(false)
+  }
+
+  const handleClickOutside = (event: React.BaseSyntheticEvent | MouseEvent): void => {
+    if (dropdownRef.current != null) {
+      const target = event.target as HTMLButtonElement
+      if (dropdownClick && !dropdownRef.current.contains(target)) {
+        setDropdownClick(false)
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (dropdownClick) document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  })
+
   return (
     <nav className={styles.gnb}>
-      <div className={styles.left}>
+      <div className={styles.top}>
         <h1>
-          <a href='/'>
+          <Link to='/'>
             <LogoImage className={styles.logoImg} />
-          </a>
+          </Link>
         </h1>
         <div className={styles.serviceWrap}>
           <p className={styles.subTitle}>서비스</p>
           <div className={styles.dropdownWrap}>
-            <select>
-              <option>매드업</option>
-              <option>서비스 추가하기</option>
-            </select>
+            <button type='button' className={styles.dropdown} onClick={handleDropdownClick}>
+              {selectedOption}
+            </button>
+            <ul
+              className={styles.dropdownListWrap}
+              style={dropdownClick ? { display: 'block' } : { display: 'none' }}
+              ref={dropdownRef}
+            >
+              <li>
+                <button type='button' onClick={handleOptionClick}>
+                  매드업
+                </button>
+              </li>
+              <li>
+                <button type='button' onClick={handleOptionClick}>
+                  서비스 추가하기
+                </button>
+              </li>
+            </ul>
             <ArrowIcon className={styles.arrowIcon} />
           </div>
         </div>
@@ -28,20 +76,20 @@ const GNB = () => {
           <ul className={styles.navWrap}>
             <li>
               <NavLink to='/' className={({ isActive }) => cx({ [styles.active]: isActive })}>
-                <DashboardIcon className={styles.dashboardIcon} />
+                <DashboardIcon className={styles.icon} />
                 <span>대시보드</span>
               </NavLink>
             </li>
             <li>
-              <NavLink to='/' className={({ isActive }) => cx({ [styles.active]: isActive })}>
-                <ChartIcon className={styles.chartIcon} />
+              <NavLink to='/a' className={({ isActive }) => cx({ [styles.active]: isActive })}>
+                <ChartIcon className={styles.icon} />
                 <span>광고관리</span>
               </NavLink>
             </li>
           </ul>
         </div>
       </div>
-      <div className={styles.right}>
+      <div className={styles.bottom}>
         <button type='button' className={styles.guideWrap}>
           <div className={styles.guideIconWrap}>
             <GuideIcon className={styles.guideIcon} />
