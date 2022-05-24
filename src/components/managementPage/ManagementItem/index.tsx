b4ba50069e22c33b1008d6aa7e86a50731453e39
saiani1 <saiani@naver.com>
2022-05-24 19:56:34 +0900

    feat: styling datepicker

---
+++ src/components/managementPage/ManagementItem/index.tsx
@@ -0,0 +1,57 @@
+import { IDataSet } from 'type/adListDataSet'
+
+import styles from './managementItem.module.scss'
+
+const ManagementItem = ({ adType, title, budget, status, startDate, endDate, report }: IDataSet) => {
+  return (
+    <div className={styles.item}>
+      <h1 className={styles.item__title}>
+        {adType}_{title}
+      </h1>
+      <div className={styles.divider} />
+      <div className={styles.item__content}>
+        <h1>상태</h1>
+        <p>{status}</p>
+      </div>
+      <div className={styles.divider} />
+
+      <div className={styles.item__content}>
+        <h1>광고 생성일</h1>
+        <p>
+          {startDate} {endDate && `(${endDate})`}
+        </p>
+      </div>
+      <div className={styles.divider} />
+
+      <div className={styles.item__content}>
+        <h1>일 희망 예산</h1>
+        <p>{budget}</p>
+      </div>
+      <div className={styles.divider} />
+
+      <div className={styles.item__content}>
+        <h1>광고 수익률</h1>
+        <p>{report.roas} %</p>
+      </div>
+      <div className={styles.divider} />
+
+      <div className={styles.item__content}>
+        <h1>매출</h1>
+        <p>{report.convValue}</p>
+      </div>
+      <div className={styles.divider} />
+
+      <div className={styles.item__content}>
+        <h1>광고 비용</h1>
+        <p>{report.cost}</p>
+      </div>
+      <div className={styles.divider} />
+
+      <button type='button' className={styles.item__editBtn}>
+        수정하기
+      </button>
+    </div>
+  )
+}
+
+export default ManagementItem
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,8 @@ import GNB from 'routes/_shared/GNB'
 import Header from 'components/Header/index'
 
 import styles from './routes.module.scss'
+import ManagementPage from 'components/managementPage'
+import { Status } from 'components/Status'
 
 const App = () => {
   return (
@@ -11,7 +13,8 @@ const App = () => {
       <div className={styles.app}>
         <Header />
         <Routes>
-          <Route path='/' />
+          <Route path='/' element={<Status />} />
+          <Route path='management' element={<ManagementPage />} />
         </Routes>
       </div>
     </div>
